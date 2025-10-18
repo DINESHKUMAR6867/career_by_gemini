ab4ceab3f683d7b3cd47561b341c2c85cc3537e5
Dinesh Kumar <168905064+DINESHKUMAR6867@users.noreply.github.com>
2025-10-18 16:13:17 +0530

    Update wsgi.py

--- career_cast/wsgi.py
+++ career_cast/wsgi.py
@@ -1,24 +1,10 @@
-
-"""
-WSGI config for career_cast project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
+from vercel import Vercel  # If you're deploying on Vercel
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'career_cast.settings')
 
 application = get_wsgi_application()
 
-handler = application
 if __name__ == "__main__":
-    Vercel(application)
-
-
-
-
+    Vercel(application)  # For local testing with Vercel
