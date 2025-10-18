ec913e30bbb3bdfd797f1e7ca5ef21e4305cd033
Dinesh Kumar <168905064+DINESHKUMAR6867@users.noreply.github.com>
2025-10-18 17:13:00 +0530

    Update wsgi.py

--- career_cast/wsgi.py
+++ career_cast/wsgi.py
@@ -1,10 +1,11 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from vercel import Vercel  # If you're deploying on Vercel
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'career_cast.settings')
 
 application = get_wsgi_application()
 
 app = application
+
 
