db0aabdb792907f171f1fc4eabce6cc17b369f86
Dinesh Kumar <168905064+DINESHKUMAR6867@users.noreply.github.com>
2025-10-19 22:41:06 +0530

    Update wsgi.py

--- career_cast/wsgi.py
+++ career_cast/wsgi.py
@@ -2,5 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "career_cast.settings")
-app = get_wsgi_application()
 
+# Vercel expects this name
+app = get_wsgi_application()
