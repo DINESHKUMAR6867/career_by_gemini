b2ea19733900d5f3458523a615f0aa3aa21c39e1
Dinesh Kumar <168905064+DINESHKUMAR6867@users.noreply.github.com>
2025-10-21 10:26:29 +0530

    Update wsgi.py

--- career_cast/wsgi.py
+++ career_cast/wsgi.py
@@ -5,3 +5,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "career_cast.settings")
 
 # Vercel expects this name
 app = get_wsgi_application()
+
+try:
+    from django.core.management import call_command
+    call_command("migrate", interactive=False, verbosity=0)
+except Exception as e:
+    print("Migrations skipped:", e)
