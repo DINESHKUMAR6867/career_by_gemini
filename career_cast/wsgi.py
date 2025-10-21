28a9d770a431e07fa70a7e745cd6b7ffa003f29c
Dinesh Kumar <168905064+DINESHKUMAR6867@users.noreply.github.com>
2025-10-21 23:01:05 +0530

    Update wsgi.py

--- career_cast/wsgi.py
+++ career_cast/wsgi.py
@@ -1,13 +1,27 @@
+# import os
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "career_cast.settings")
+
+# # Vercel expects this name
+# app = get_wsgi_application()
+
+# try:
+#     from django.core.management import call_command
+#     call_command("migrate", interactive=False, verbosity=0)
+# except Exception as e:
+#     print("Migrations skipped:", e)
+
 import os
 from django.core.wsgi import get_wsgi_application
+from django.core.management import execute_from_command_line
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "career_cast.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'career_cast.settings')
 
-# Vercel expects this name
-app = get_wsgi_application()
-
+# Try to run migrations on startup
 try:
-    from django.core.management import call_command
-    call_command("migrate", interactive=False, verbosity=0)
-except Exception as e:
-    print("Migrations skipped:", e)
+    execute_from_command_line(['manage.py', 'migrate', '--noinput'])
+except:
+    pass  # Ignore errors if tables already exist
+
+app = get_wsgi_application()
